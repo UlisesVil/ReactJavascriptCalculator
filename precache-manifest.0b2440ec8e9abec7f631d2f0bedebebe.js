7dae3ef1eb7ab5f64253fb8a74c93ad441b27fe2
github_ulisesvil <ulisesvil5@hotmail.com>
2020-07-14 04:31:28 -0500

    Updates

---
+++ precache-manifest.0b2440ec8e9abec7f631d2f0bedebebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a875031334902c1bdf8b5591941d757",
+    "url": "/ReactJavascriptCalculator/index.html"
+  },
+  {
+    "revision": "352176d91cf75c00c687",
+    "url": "/ReactJavascriptCalculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f03809d671dd8c4c5389",
+    "url": "/ReactJavascriptCalculator/static/js/2.e9ae1c80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJavascriptCalculator/static/js/2.e9ae1c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352176d91cf75c00c687",
+    "url": "/ReactJavascriptCalculator/static/js/main.2b564c2b.chunk.js"
+  },
+  {
+    "revision": "89f1e7b1b8496f55ac0a",
+    "url": "/ReactJavascriptCalculator/static/js/runtime-main.9cc098e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJavascriptCalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
